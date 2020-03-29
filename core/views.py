7b2b5b04d787e797c8275124db2fce0351c5bb7f
sanjayyr <sanjayyriit@gmail.com>
2020-03-29 17:25:22 -0500

    SanjayEcommerce: Add home page and product view

--- core/views.py
+++ core/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+from django.views.generic import ListView, DetailView
 
 from .models import Item
 
-def item_list(request):
-	context = {
-		'items': Item.objects.all()
-	}
-	return render(request, "home-page.html", context)
+
+def checkout(request):
+	return render(request, 'checkout.html')
+
+class HomeView(ListView):
+	model = Item
+	template_name = "home.html"
+
+class ItemDetailView(DetailView):
+	model = Item
+	template_name = "product.html"	
+
